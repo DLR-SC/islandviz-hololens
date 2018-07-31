fb164547035ab832fb3869cc54a1c889cde3724e
Artur Baranowski <artur.baranowski90@gmail.com>
2018-07-31 17:02:02 +0200

    Added scan instructions to UI.

--- Assets/Scripts/Component/UI/ScanInstructionText.cs
+++ Assets/Scripts/Component/UI/ScanInstructionText.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
+﻿using HoloIslandVis.Mapping;
+using HoloToolkit.Unity;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScanInstructionText : MonoBehaviour {
+public class ScanInstructionText : MonoBehaviour
+{
+    private int _platformCellCount;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public TextMesh TextMesh;
+
+    private void OnEnable()
+    {
+        SpatialScan.Instance.PlatformCellCountObservable.ValueChanged += OnPlatformCellCountChanged;
+        SpatialScan.Instance.ScanStateObservable.ValueChanged += OnScanStateChanged;
+    }
+
+    private void OnDisable()
+    {
+        SpatialScan.Instance.PlatformCellCountObservable.ValueChanged -= OnPlatformCellCountChanged;
+        SpatialScan.Instance.ScanStateObservable.ValueChanged -= OnScanStateChanged;
+    }
+
+    private void OnPlatformCellCountChanged(int platformCellCount)
+    {
+        _platformCellCount = platformCellCount;
+        if(_platformCellCount < SpatialScan.Instance.TargetPlatformCellCount)
+            TextMesh.text = "Walk around and scan room.";
+        else if(_platformCellCount >= SpatialScan.Instance.TargetPlatformCellCount)
+            TextMesh.text = "Tap to finalize scan.";
+    }
+
+    private void OnScanStateChanged(SpatialUnderstanding.ScanStates scanState)
+    {
+        //throw new NotImplementedException();
+    }
 }
