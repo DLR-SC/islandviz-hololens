6fe34423829d35df1ad9f26d7ba6268f9b883aef
Artur Baranowski <Artur.Baranowski@dlr.de>
2019-07-25 07:18:20 +0200

    Added functions to move down, left, and right via speech control.

--- Assets/Scripts/Core/AppManager.cs
+++ Assets/Scripts/Core/AppManager.cs
@@ -153,6 +153,7 @@ namespace HoloIslandVis.Core
             TaskToggleDependency task_toggleDependency  = new TaskToggleDependency();
             TaskResetPane task_resetPane                = new TaskResetPane();
             TaskMoveDirection task_moveDirection        = new TaskMoveDirection();
+            TaskZoom task_zoom                          = new TaskZoom();
 
             Command command_dragProjected       = new Command(GestureType.OneHandManipStart, KeywordType.Invariant, InteractableType.Invariant);
             Command command_zoomProjected       = new Command(GestureType.TwoHandManipStart, KeywordType.Invariant, InteractableType.Invariant);
@@ -161,7 +162,12 @@ namespace HoloIslandVis.Core
             Command command_importSelectGesture = new Command(GestureType.OneHandTap, KeywordType.Invariant, InteractableType.ExportDock);
             Command command_islandSelectSpeech  = new Command(GestureType.None, KeywordType.Select, InteractableType.Bundle);
             Command command_resetPane           = new Command(GestureType.None, KeywordType.Select, InteractableType.ContentPane);
+            Command command_zoomIn              = new Command(GestureType.None, KeywordType.ZoomIn, InteractableType.Invariant);
+            Command command_zoomOut             = new Command(GestureType.None, KeywordType.ZoomOut, InteractableType.Invariant);
             Command command_moveUp              = new Command(GestureType.None, KeywordType.MoveUp, InteractableType.Invariant);
+            Command command_moveDown            = new Command(GestureType.None, KeywordType.MoveDown, InteractableType.Invariant);
+            Command command_moveLeft            = new Command(GestureType.None, KeywordType.MoveLeft, InteractableType.Invariant);
+            Command command_moveRight           = new Command(GestureType.None, KeywordType.MoveRight, InteractableType.Invariant);
             Command command_adjust              = new Command(StaticItem.Adjust);
             Command command_showDependencies    = new Command(StaticItem.Dependencies);
             Command command_fitContent          = new Command(StaticItem.Fit);
@@ -175,7 +181,12 @@ namespace HoloIslandVis.Core
             state_main.AddInteractionTask(command_importSelectGesture, task_toggleDependency);
             state_main.AddInteractionTask(command_exportSelectGesture, task_toggleDependency);
             state_main.AddInteractionTask(command_resetPane, task_resetPane);
+            state_main.AddInteractionTask(command_zoomIn, task_zoom);
+            state_main.AddInteractionTask(command_zoomOut, task_zoom);
             state_main.AddInteractionTask(command_moveUp, task_moveDirection);
+            state_main.AddInteractionTask(command_moveDown, task_moveDirection);
+            state_main.AddInteractionTask(command_moveLeft, task_moveDirection);
+            state_main.AddInteractionTask(command_moveRight, task_moveDirection);
 
             state_main.AddStateTransition(command_adjust, state_init);
             state_main.AddStateTransition(command_islandSelectGesture, state_inspectIsland);
--- Assets/Scripts/Interaction/Tasking/Task/TaskMoveDirection.cs
+++ Assets/Scripts/Interaction/Tasking/Task/TaskMoveDirection.cs
@@ -17,36 +17,72 @@ namespace HoloIslandVis.Interaction.Tasking.Task
 
         public override IEnumerator Perform(SpeechInteractionEventArgs eventArgs)
         {
+            // Number of seconds to perform a shift.
             _duration = 4.0f;
+            // Influences the width of the shift.
             _scalingFactor = 20.0f;
+
             _visualization = UIManager.Instance.GetUIElement(UIElement.Visualization);
             _contentPane = UIManager.Instance.GetUIElement(UIElement.ContentPane);
+
+            // Get position information
             Vector3 headPosition = GazeManager.Instance.GazeOrigin;
             Vector3 contentPosition = _contentPane.transform.position;
+
+            // Project the vector from head to content into the plane. 
             Vector3 paneDirection = -(headPosition - contentPosition);
             float heightFactor = Vector3.Dot(_contentPane.transform.up, paneDirection);
             Vector3 height = _contentPane.transform.up * heightFactor;
             Vector3 projectedDirection = Vector3.Normalize(paneDirection - height);
 
+            // The width of the shift is adapted to the zoom level of the application.
+            // scaledMovement stores an according factor.
+            Vector3 scaledMovement;
+            // Start and target position of the shift.
+            Vector3 startPosition = _visualization.transform.position;
+            Vector3 targetPosition;
+
+            // Used when moving to the left or to the right.
+            Vector3 directionShift;
+
+            // Calculate the target position depending on the direction.
             switch (eventArgs.Keyword)
             {
-                case KeywordType.MoveRight: yield return MoveRight(projectedDirection); break;
-                case KeywordType.MoveLeft: yield return MoveLeft(projectedDirection); break;
-                case KeywordType.MoveDown: yield return MoveDown(projectedDirection); break;
-                case KeywordType.MoveUp: yield return MoveUp(projectedDirection); break;
-                default: break;
+                case KeywordType.MoveRight:
+                    directionShift = Vector3.Cross(projectedDirection, _contentPane.transform.up).normalized;
+                    break;
+                case KeywordType.MoveLeft:
+                    directionShift = Vector3.Cross(_contentPane.transform.up, projectedDirection).normalized;
+                    break;
+                case KeywordType.MoveDown:
+                    directionShift = projectedDirection;
+                    break;
+                case KeywordType.MoveUp:
+                    directionShift = projectedDirection;
+                    break;
+                default:
+                    directionShift = Vector3.zero;
+                    break;
             }
+            scaledMovement = directionShift * _visualization.transform.localScale.x;
+            targetPosition = startPosition + scaledMovement * _scalingFactor;
+            yield return ShiftPane(startPosition, targetPosition);
         }
 
-        private IEnumerator MoveUp(Vector3 lookDirection)
+        private IEnumerator ShiftPane(Vector3 startPosition, Vector3 targetPosition)
         {
-            Vector3 scaledMovement = lookDirection * _visualization.transform.localScale.x;
-
-            Vector3 startPosition = _visualization.transform.position;
-            Vector3 targetPosition = startPosition - scaledMovement * _scalingFactor;
             Vector3 currentPosition = startPosition;
 
+            /* 
+             * (cumulative_time - duration / 2) - 12 / duration 
+             * 
+             * This function takes the time that has passed since the beginning of the move process
+             * and maps it to input range of -6 to 6. This input range is chosen, as the sigmoid function
+             * converges to -1 or 1 outside of this area.
+             */
+
             float interpolation = 0.0f;
+            // Time passed since the beginning of the move process
             float cumulativeTime = 0.0f;
             float durationOffset = _duration / 2.0f;
             float durationFactor = 12.0f / _duration;
@@ -64,25 +100,9 @@ namespace HoloIslandVis.Interaction.Tasking.Task
             _visualization.transform.position = targetPosition;
         }
 
-        private IEnumerator MoveDown(Vector3 lookDirection)
-        {
-            Vector3 scaledMovement = lookDirection * _visualization.transform.localScale.x;
-            _visualization.transform.position += scaledMovement * _scalingFactor;
-            yield return null;
-        }
-
-        private IEnumerator MoveLeft(Vector3 lookDirection)
-        {
-            yield return null;
-        }
-
-        private IEnumerator MoveRight(Vector3 lookDirection)
-        {
-            yield return null;
-        }
-
         public float Sigmoid(float x)
         {
+            // Results of the sigmoid function are in the interval of (0, 1)
             return (1 / (1 + Mathf.Exp(-x)));
         }
     }
